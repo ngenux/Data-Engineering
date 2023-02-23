c3cb80fb0756a9af83795387cccd1a226b3446f9
ng-ChaitanyaVasa <chaitanya.vasa@ngenux.com>
2023-02-23 20:04:20 +0530

    adding pyspark etl example

---
+++ Upskill/Pyspark/pyspark_examples/spark_etl.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+import pandas as pd
+import pymysql
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages "org.apache.hadoop:hadoop-aws:3.3.1" pyspark-shell'
+
+# Set up the SparkSession with the appropriate configurations for S3
+spark = SparkSession.builder \
+    .appName('s3-read') \
+    .config('spark.hadoop.fs.s3a.access.key', "AKIAYJC5TJLTWRIQTYHI") \
+    .config('spark.hadoop.fs.s3a.secret.key', "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") \
+    .config('spark.hadoop.fs.s3a.endpoint', 's3.amazonaws.com') \
+    .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem') \
+    .getOrCreate()
+conn = pymysql.connect(user='devuser', database='practice',
+                               password='Chaitu2798!',
+                               host="localhost",
+                               port=3306)
+cursor = conn.cursor()
+query = "SELECT * FROM orders1"
+query1 = 'SELECT * FROM products1'
+# Create a pandas dataframe
+pdf = pd.read_sql(query, con=conn)
+pdf1 = pd.read_sql(query1, con=conn)
+conn.close()
+# Convert Pandas dataframe to spark DataFrame
+order_df = spark.createDataFrame(pdf)
+products_df = spark.createDataFrame(pdf1)
+df_join = products_df.join(order_df, order_df.product_id == products_df.product_id, "inner") \
+    .drop(order_df.product_id)
+df_join.show()
+
+# writing the data
+df_join.write.format('csv').option('header','true') \
+    .save('s3a://sparketl/pyspark/data.csv',mode='overwrite')
+# reading the data
+s3_df = spark.read.csv("s3a://sparketl/pyspark/data.csv/",header=True,inferSchema=True)
+s3_df.show()
